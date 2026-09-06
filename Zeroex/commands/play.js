a2d26873846a0732c6f71e1af28bfbd19814e62d
piku-123 <zeroex143@gmail.com>
2026-09-06 11:53:29 +0600

    Update play.js DL Api change from yt-mp3 to yt/down

--- Zeroex/commands/play.js
+++ Zeroex/commands/play.js
@@ -3,10 +3,18 @@ const path = require("path");
 const axios = require("axios");
 
 async function downloadMusic(videoID, filePath) {
-  const apiUrl = `https://zeroex-tools.onrender.com/api/yt-mp3?url=https://www.youtube.com/watch?v=${videoID}`;
+  const apiUrl = `https://zeroex-tools.onrender.com/api/yt/down?url=https://www.youtube.com/watch?v=${videoID}&format=mp3`;
+  const res = await axios.get(apiUrl);
+
+  if (!res.data || !res.data.status || !res.data.result || !res.data.result.downloadUrl) {
+    throw new Error("Failed to get download URL from API");
+  }
+
+  const downloadUrl = res.data.result.downloadUrl;
+
   const response = await axios({
     method: "get",
-    url: apiUrl,
+    url: downloadUrl,
     responseType: "stream"
   });
 
